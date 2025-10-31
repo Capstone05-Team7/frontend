e790e66c5cd81ba32f46a6b7dcfc7c9305d8728c
yc3697 <youngchan9683@gmail.com>
2025-10-31 15:23:58 +0900

    feat: 베포 서버 작동을 위한 base url 임시 변경 #3

--- app/src/main/java/com/example/capstone07/NetworkModule.kt
+++ app/src/main/java/com/example/capstone07/NetworkModule.kt
@@ -6,11 +6,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object NetworkModule {
 
     // 애뮬레이터 사용 시
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    //private const val BASE_URL = "http://10.0.2.2:8080/"
 
     // 실제 안드로이드 기기 사용 시
     // PC와 폰이 동일한 wifi에 연결되어 있어야 함
-    // private const val BASE_URL = "http://<PC의_실제_IP>:8080/"
+    //private const val BASE_URL = "http://127.0.0.1:8080" // "http://<PC의_실제_IP>:8080/"
+
+    // ec2 서버
+    private const val BASE_URL = "http://13.209.4.95:8080"
 
     private var retrofit: Retrofit? = null
 
