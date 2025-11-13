c504c5db0d1ffc9fba2866864080a72fe0f69d80
keypang <bestnest820@naver.com>
2025-11-13 13:37:15 +0900

    feat: 키워드 표시UI 변경

--- app/src/main/java/com/example/capstone07/ui/analysis/AnalysisAdapter.kt
+++ app/src/main/java/com/example/capstone07/ui/analysis/AnalysisAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.capstone07.ui.analysis
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.TextView
@@ -20,7 +24,8 @@ class AnalysisAdapter(private var scripts: List<ScriptResponseFragment> = emptyL
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val scriptTextView: TextView = itemView.findViewById(R.id.tvScriptText)
-        val gridKeywords: GridLayout = itemView.findViewById(R.id.gridKeywords)
+        val keywordTextView: TextView = itemView.findViewById(R.id.tvKeywordText)
+        val keywordEditText: EditText = itemView.findViewById(R.id.etKeywordText)
     }
 
 
@@ -31,22 +36,62 @@ class AnalysisAdapter(private var scripts: List<ScriptResponseFragment> = emptyL
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val scriptText = "안녕하세요. 지금부터 kbo의 역사에 대해서 소개하겠습니다."
-//        val keywords = listOf("안녕", "kbo", "역사", "소개")
+        //val scriptText = "안녕하세요. 지금부터 kbo의 역사에 대해서 소개하겠습니다."
+        //val keywords = listOf("안녕", "kbo", "역사", "소개")
         val script = scripts[position]
 
         holder.scriptTextView.text = script.sentenceFragmentContent
 
         // 키워드 버튼
-        holder.gridKeywords.removeAllViews()
-        val keywordBtn = Button(holder.itemView.context).apply {
-            text = script.keyword
-            setTextColor(Color.parseColor("#205CFF"))
-            textSize = 15f
-        }
-        holder.gridKeywords.addView(keywordBtn)
+        holder.keywordTextView.visibility = View.VISIBLE
+        holder.keywordEditText.visibility = View.GONE
 
-        println("Binding item at position: $position")
+        holder.keywordTextView.text = script.keyword
+        holder.keywordEditText.setText(script.keyword)
+
+        holder.keywordTextView.setOnClickListener {
+            holder.keywordEditText.setText(holder.keywordTextView.text)
+
+            holder.keywordTextView.visibility = View.GONE
+            holder.keywordEditText.visibility = View.VISIBLE
+
+            holder.keywordEditText.requestFocus()
+            showKeyboard(holder.keywordEditText)
+        }
+
+        holder.keywordEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val modifiedText = holder.keywordEditText.text.toString()
+
+                scripts[position].keyword = modifiedText
+
+                holder.keywordTextView.text = modifiedText
+
+                holder.keywordEditText.visibility = View.GONE
+                holder.keywordTextView.visibility = View.VISIBLE
+
+                hideKeyboard(holder.keywordEditText)
+                true
+            } else {
+                false
+            }
+        }
+
+        holder.keywordEditText.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus && holder.keywordEditText.visibility == View.VISIBLE) {
+                val modifiedText = holder.keywordEditText.text.toString()
+
+                scripts[position].keyword = modifiedText
+
+                holder.keywordTextView.text = modifiedText
+                holder.keywordEditText.visibility = View.GONE
+                holder.keywordTextView.visibility = View.VISIBLE
+
+                hideKeyboard(holder.keywordEditText)
+            }
+        }
+
+
     }
 
     override fun getItemCount(): Int = scripts.size
@@ -55,5 +100,21 @@ class AnalysisAdapter(private var scripts: List<ScriptResponseFragment> = emptyL
     fun setScripts(newScripts: List<ScriptResponseFragment>) {
         scripts = newScripts
         notifyDataSetChanged()
+    }
+
+
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    // (참고: 키보드를 올리는 함수)
+    fun showKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            view.postDelayed({
+                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+            }, 100)
+        }
     }
 }
